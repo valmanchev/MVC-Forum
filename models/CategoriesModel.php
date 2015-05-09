a96846846dfb3b69a76e6ad6937cbe2eb882a661
Valeri Manchev <vmanchev@yahoo.com>
2015-05-09 18:07:20 +0300

    fixed filter bug

--- models/CategoriesModel.php
+++ models/CategoriesModel.php
@@ -28,7 +28,12 @@ class CategoriesModel extends BaseModel {
     }
 
     public function getQuestions($id) {
-        $statement = self::$db->prepare("SELECT * FROM questions WHERE category_id = ? ORDER BY id DESC");
+        $statement = self::$db->prepare("SELECT q.id, q.name, q.counter, c.name, t.name
+                                            FROM questions q
+                                            JOIN categories c
+                                            ON q.category_id = c.id
+                                            JOIN tags t
+                                            ON q.tag_id = t.id WHERE q.category_id = ? ORDER BY id DESC");
         $statement->bind_param("i", $id);
         $statement->execute();
         $result = $statement->get_result()->fetch_all();
